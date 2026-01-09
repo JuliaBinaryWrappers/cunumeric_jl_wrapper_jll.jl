ee6328c2edb84d984642ac18f590c630e8d1db48
jlbuild <juliabuildbot@gmail.com>
2026-01-09 14:43:15 +0000

    cunumeric_jl_wrapper_jll build 25.10.1+0

--- src/cunumeric_jl_wrapper_jll.jl
+++ src/cunumeric_jl_wrapper_jll.jl
@@ -7,5 +7,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("cunumeric_jl_wrapper")
-JLLWrappers.@generate_main_file("cunumeric_jl_wrapper", UUID("49048992-29d2-5fd1-994f-9cecf112d624"))
+JLLWrappers.@generate_main_file("cunumeric_jl_wrapper", Base.UUID("49048992-29d2-5fd1-994f-9cecf112d624"))
 end  # module cunumeric_jl_wrapper_jll
